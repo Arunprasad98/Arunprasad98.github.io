7764df93dabcf6f77ff645466d04f00cccdf92d8
Arunprasad R <51456421+Arunprasad98@users.noreply.github.com>
2020-07-19 23:50:54 +0530

    Update .js

--- .js
+++ .js
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>X-Frame-Bypass For Global CI calendar</title>
-    <style>
-        html, body {
-            margin: 0;
-            padding: 0;
-            height: 100%;
-            overflow: hidden;
-        }
-        iframe {
-            display: block;
-            width: calc(100% - 40px);
-            height: calc(100% - 40px);
-            margin: 20px;
-            border: 0;
-        }
-
-    </style>
-
-    <!-- To bypass the CROSS ORIGIN Resource issue -->
-        <script src="x-frame-bypass.js" type="module"></script>
-
-    <!-- Support Customized built-in elements for Safari-->
-        <script src="https://github.com/Arunprasad98/Arunprasad98.github.io/blob/master/index.html"></script>
-
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="X-Frame-Bypass: Web Component extending IFrame to bypass X-Frame-Options: deny/sameorigin">
+	<title>X-Frame-Bypass Web Component Demo</title>
+	<style>
+		html, body {
+			margin: 0;
+			padding: 0;
+			height: 100%;
+			overflow: hidden;
+		}
+		iframe {
+			display: block;
+			width: calc(100% - 40px);
+			height: calc(100% - 40px);
+			margin: 20px;
+		}
+		img {
+			position: absolute;
+			top: 0;
+			right: 0;
+		}
+	</style>
+	<script src="https://unpkg.com/@ungap/custom-elements-builtin"></script>
+	<script src="x-frame-bypass.js" type="module"></script>
 </head>
 <body>
-    <iframe is="x-frame-bypass" src="https://github.com/Arunprasad98/Arunprasad98.github.io/blob/master/XR%20Animation.2.html"></iframe>
+	<iframe is="x-frame-bypass" src="https://news.ycombinator.com/"></iframe>
+	<a href="https://github.com/Arunprasad98/Arunprasad98.github.io/blob/master/X%20Frame%20bypass.js"><img src="https://github.com/Arunprasad98/Arunprasad98.github.io/blob/master/XR%20Animation.2.html" alt="Fork me on GitHub"></a>
 </body>
 </html>
